9ab12df1d7a6f83cc82fe25c5f4b0c49e4caa405
Yash <yashkd12790@gmail.com>
2024-06-21 20:15:37 +0530

    add navbar and messages card

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,6 +1,78 @@
-import React from "react";
+"use client";
+import { useToast } from "@/components/ui/use-toast";
+import { Message } from "@/model/User";
+import { acceptMessageSchema } from "@/schemas/acceptMessageSchema";
+import { ApiResponse } from "@/types/ApiResponse";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios, { AxiosError } from "axios";
+import { useSession } from "next-auth/react";
+import React, { useCallback, useState } from "react";
+import { useForm } from "react-hook-form";
 
 const page = () => {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [isloading, setIsloading] = useState(false);
+  const [isSwitchLoading, setIsSwitchLoading] = useState(false);
+
+  const { toast } = useToast();
+
+  const handleDeleteMessage = (messageId: string) => {
+    setMessages(messages.filter((message) => message._id !== messageId));
+  };
+
+  const { data: session } = useSession();
+
+  const form = useForm({ resolver: zodResolver(acceptMessageSchema) });
+
+  const { register, watch, setValue } = form;
+
+  const acceptMessage = watch("acceptMessage");
+
+  const fetchAcceptMessages = useCallback(async () => {
+    setIsSwitchLoading(true);
+
+    try {
+      const response = await axios.get<ApiResponse>("/api/accept-messages");
+      setValue("acceptMessage", response.data.isAcceptingMessages);
+    } catch (error) {
+      const axiosError = error as AxiosError<ApiResponse>;
+      toast({
+        title: "Error",
+        description:
+          axiosError.response?.data.message ||
+          "failed to fetch message setting",
+        variant: "destructive",
+      });
+    } finally {
+      setIsSwitchLoading(false);
+    }
+  }, [setValue]);
+  const fetchMessages = useCallback(async (refresh: boolean = false) => {
+    setIsloading(true);
+    setIsSwitchLoading(false);
+
+    try {
+      const response = await axios.get<ApiResponse>("api/get-messages");
+
+      setMessages(response.data.messages || []);
+
+      if (refresh) {
+        toast({
+          title: "refreshed messages",
+          description: "showing latest messages",
+        });
+      }
+    } catch (error) {
+      const axiosError = error as AxiosError<ApiResponse>;
+      toast({
+        title: "Error",
+        description:
+          axiosError.response?.data.message ||
+          "failed to fetch message setting",
+        variant: "destructive",
+      });
+    }
+  }, []);
   return <div>sa</div>;
 };
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "./globals.css";
 
+import AuthProvider from "@/context/AuthProvider";
+import { Toaster } from "@/components/ui/toaster";
 import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,10 +20,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Navbar />
-        {children}
-      </body>
+      <AuthProvider>
+        <body className={inter.className}>
+          {children}
+          <Toaster />
+        </body>
+      </AuthProvider>
     </html>
   );
 }
