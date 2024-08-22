123a8d63bdf5edd85bab6f670fef67d28f732f9d
Yash <yashkd12790@gmail.com>
2024-08-22 19:10:06 +0530

    alignment fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,14 +11,16 @@ function Navbar() {
   const user: User = session?.user;
 
   return (
-    <nav className="p-4 md:p-6 shadow-md bg-gray-900 text-white">
+    <nav className="p-2 md:p-6 shadow-md bg-gray-900 text-white">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <a href="#" className="text-xl font-bold mb-4 md:mb-0">
+        <a href="/" className="text-xl font-bold mb-4 md:mb-0">
           No hard feelings Feedback
         </a>
         {session ? (
           <>
-            <span className="mr-4">Welcome, {user.username || user.email}</span>
+            <span className=" md:mr-32 self-center  ">
+              Welcome, {user.username || user.email}
+            </span>
             <Button
               onClick={() => signOut()}
               className="w-full md:w-auto bg-slate-100 text-black"
